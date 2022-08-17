f438064b68ab22577d5e2456e5bca0014e83b524
Marco Gutierrez <a20141676@pucp.pe>
2022-08-17 18:55:56 -0500

    refactored 02_construct_datasets for creating post treatment datasets (todo: add date filter)

--- case_outcomes/02_construct_datasets.R
+++ case_outcomes/02_construct_datasets.R
@@ -162,7 +162,6 @@ length_resolution <- data_participant_caseid %>%
 # For post-treatment regression
 
 fundada_appeal_var <- data_participant_caseid %>%
-  filter(sentence_after_pca == 1) %>%
   group_by(participant_id, month_year_sentence) %>%
   summarise(
     var_fundada = mean(var_fundada, na.rm = TRUE),
@@ -172,7 +171,6 @@ fundada_appeal_var <- data_participant_caseid %>%
   ungroup()
 
 resolution_var <- data_participant_caseid %>%
-  filter(first_auto_after_pca == 1) %>%
   group_by(participant_id, month_year_first_auto) %>%
   summarise(
     percent_monit = first(percent_monit),
@@ -183,7 +181,6 @@ resolution_var <- data_participant_caseid %>%
   std_outcome(var_resolution, var_resolution_std, percent_monit)
 
 days_to_res_var <- data_participant_caseid %>%
-  filter(resolution_after_pca == 1) %>%
   group_by(participant_id, month_year_resolution) %>%
   summarise(
     percent_monit = first(percent_monit),
@@ -191,48 +188,6 @@ days_to_res_var <- data_participant_caseid %>%
     length_auto_resolution = mean(length_auto_resolution, na.rm = TRUE)) %>%
   rename(month_year = month_year_resolution) %>%
   ungroup() %>%
-  # Standardize outcome
-  std_outcome(var_timely_resolved, var_timely_resolved_std, percent_monit) %>%
-  std_outcome(length_auto_resolution, length_auto_resolution_std, percent_monit)
-
-# Create case outcomes variables at the month-judge level
-# for DiD regression
-
-fundada_appeal_var_did <- data_participant_caseid %>%
-  filter(date_sentence > ymd(20180501)) %>%
-  filter(!sentence_during_pca == 1) %>%
-  group_by(participant_id, month_year_sentence) %>%
-  summarise(
-    sentence_after_pca = first(sentence_after_pca),
-    var_fundada = mean(var_fundada, na.rm = TRUE),
-    var_uncon_reversal = mean(var_uncon_reversal, na.rm = TRUE),
-    var_appeal = mean(var_appeal, na.rm = TRUE)) %>%
-  rename(month_year = month_year_sentence)
-
-resolution_var_did <- data_participant_caseid %>%
-  filter(date_first_auto > ymd(20180501)) %>%
-  filter(!first_auto_during_pca == 1) %>%
-  group_by(participant_id, month_year_first_auto) %>%
-  summarise(
-    percent_monit = first(percent_monit),
-    first_auto_after_pca = first(first_auto_after_pca),
-    var_resolution = mean(var_resolution, na.rm = TRUE)) %>%
-  rename(month_year = month_year_first_auto) %>%
-  ungroup()  %>%
-  # Standardize outcome
-  std_outcome(var_resolution, var_resolution_std, percent_monit)
-
-days_to_res_var_did <- data_participant_caseid %>%
-  filter(date_resolution > ymd(20180501)) %>%
-  filter(!resolution_during_pca == 1) %>%
-  group_by(participant_id, month_year_resolution) %>%
-  summarise(
-    percent_monit = first(percent_monit),
-    resolution_after_pca = first(resolution_after_pca),
-    var_timely_resolved = mean(timely_resolved, na.rm = TRUE),
-    length_auto_resolution = mean(length_auto_resolution, na.rm = TRUE)) %>%
-  rename(month_year = month_year_resolution) %>%
-  ungroup()  %>%
   # Standardize outcome
   std_outcome(var_timely_resolved, var_timely_resolved_std, percent_monit) %>%
   std_outcome(length_auto_resolution, length_auto_resolution_std, percent_monit)
@@ -267,34 +222,6 @@ days_to_res_data_post <- data_participant %>%
   inner_join(days_to_res_var)
 days_to_res_judges <- days_to_res_data_post %>% select(participant_id) %>% unique()
 
-# Create DiD datasets
-fundada_appeal_data_did <- data_participant %>%
-  inner_join(fundada_appeal_var_did) %>%
-  inner_join(fundada_appeal_judges) %>%
-  mutate(
-    percent_monit_full = percent_monit,
-    after_pca = sentence_after_pca,
-    percent_monit = case_when(
-    sentence_after_pca == 0 ~ 0,
-    sentence_after_pca == 1 ~ percent_monit))
-
-resolution_data_did <- data_participant %>%
-  inner_join(resolution_var_did) %>%
-  inner_join(resolution_judges)  %>%
-  mutate(
-    percent_monit_full = percent_monit,
-    percent_monit = case_when(
-    first_auto_after_pca == 0 ~ 0,
-    first_auto_after_pca == 1 ~ percent_monit))
-
-days_to_res_data_did <- data_participant %>%
-  inner_join(days_to_res_var_did) %>%
-  inner_join(days_to_res_judges) %>%
-  mutate(
-    percent_monit_full = percent_monit,
-    percent_monit = case_when(
-    resolution_after_pca == 0 ~ 0,
-    resolution_after_pca == 1 ~ percent_monit))
 
 # Save analysis datasets ----
 
@@ -318,12 +245,3 @@ write_csv(resolution_data_post,
 
 write_csv(days_to_res_data_post,
   paste0(data_amag_fin, "00_analysis/dataset_caseid_month_days_to_res_post.csv"))
-
-write_csv(fundada_appeal_data_did,
-  paste0(data_amag_fin, "00_analysis/dataset_caseid_month_fundada_did.csv"))
-
-write_csv(resolution_data_did,
-  paste0(data_amag_fin, "00_analysis/dataset_caseid_month_resolution_did.csv"))
-
-write_csv(days_to_res_data_did,
-  paste0(data_amag_fin, "00_analysis/dataset_caseid_month_days_to_res_did.csv"))
